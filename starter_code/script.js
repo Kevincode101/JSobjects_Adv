7363b584ad4800f3b00d09cf169288e38275f6d0
kevin lima <kevin.lima.code@gmail.com>
2016-11-02 21:58:29 +0000

    new changes to the html

--- starter_code/script.js
+++ starter_code/script.js
@@ -1,13 +1,13 @@
 //Task 1: Console the year in which the song "Beat it" was relased 
-
+//alert(library.music.eighties.Michael_Jackson[0].year);
 
 //Task 2: Console the name of the song Lady Gaga released in 2006 
+//alert(library.music.)
 
 
-
-$( document ).ready(function() {
+//( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
     
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
 
-});
+//});
